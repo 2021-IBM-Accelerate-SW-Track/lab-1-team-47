adf68a7b19e9079b87357005a27277e777b88f47
ddollar00 <64802602+ddollar00@users.noreply.github.com>
2021-06-19 15:44:50 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import { Component } from 'react';
-import './App.css';
 
 import React from 'react';
-
-
-
-
 import "./App.css";
 import Header from "./component/header";
 import TodoList from "./component/TodoList";
